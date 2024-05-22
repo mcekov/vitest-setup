036424c9efa58aad85f1575212d12edbf6603311
mcekov <mtsekov@zohomail.eu>
2024-05-22 23:01:21 +0300

    Add tests for ProductImageGallery Component

---
+++ tests/components/ProductImageGallery.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import ProductImageGallery from "../../src/components/ProductImageGallery";
+
+describe("ProductImageGallery", () => {
+  it("should render nothing if given an empty list", () => {
+    const { container } = render(<ProductImageGallery imageUrls={[]} />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it("should render a list of images", () => {
+    const imageUrls = ["dharmaLogo.jpg", "swan.jpg"];
+
+    render(<ProductImageGallery imageUrls={imageUrls} />);
+
+    const images = screen.getAllByRole("img");
+    expect(images).toHaveLength(2);
+    imageUrls.forEach((url, index) => {
+      expect(images[index]).toHaveAttribute("src", url);
+    });
+  });
+});
