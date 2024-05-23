4f8bb5c646f1c24f4dab024162e568e01c748a75
mcekov <mtsekov@zohomail.eu>
2024-05-23 19:10:07 +0300

    Add text in PlaygroundPage

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,11 @@
-import Onboarding from "../components/Onboarding";
+import ExpandableText from "../components/ExpandableText";
+// import Onboarding from "../components/Onboarding";
 
 const PlaygroundPage = () => {
-  return <Onboarding />;
+  return (
+    // <Onboarding />
+    <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nisi magni fugit enim ratione nesciunt rerum officia odio at error porro eligendi dolorum facere deleniti, earum dolor tempore, impedit ea repudiandae expedita. Dicta ad laboriosam sunt quia enim similique, repellat ipsum ea natus quod doloremque aut impedit rerum voluptatibus nemo sint vel, sequi eius laudantium in. Nobis debitis tenetur quidem aspernatur? Deserunt doloremque aperiam officiis quaerat vel? Ipsam asperiores aliquid temporibus, voluptatibus sint perferendis, ratione doloremque modi sunt, fuga velit laboriosam facilis hic. A eius quis voluptatibus, fugit magni velit accusantium consequatur, perferendis, reiciendis dolorum voluptatum eum praesentium expedita hic." />
+  );
 };
 
 export default PlaygroundPage;
